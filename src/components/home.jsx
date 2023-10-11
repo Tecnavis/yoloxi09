377eb15ce40ad00bc96dafdf91e58153d9516722
erkirhann <134487751+erkirhann@users.noreply.github.com>
2023-10-11 14:38:32 +0530

    .

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function home(props) {
+    return (
+        <div>
+            <h1>
+                yoloxi
+            </h1>
+        </div>
+    );
+}
+
+export default home;
